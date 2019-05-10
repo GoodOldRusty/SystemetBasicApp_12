a53d82b3cb6f8d1ab53715be16ab738324d7d9ff
Sebbe <gusskoglse@student.gu.se>
2019-05-10 10:53:27 +0200

    Id set on OnClickListener v1

--- app/src/main/java/se/juneday/systemetappbasic/LoginActivity.java
+++ app/src/main/java/se/juneday/systemetappbasic/LoginActivity.java
@@ -2,6 +2,9 @@ package se.juneday.systemetappbasic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button yesButton = findViewById(R.id.yes_button);
+        Button noButton = findViewById(R.id.no_button);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
