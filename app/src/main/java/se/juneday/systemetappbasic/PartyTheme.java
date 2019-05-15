9990ffa3b26322ce15734bfcba8eb8e711b53ee7
Evelina Bragsjö <gusbragsev@student.gu.se>
2019-05-15 14:11:46 +0200

    Created new Activity PartyTheme

---
+++ app/src/main/java/se/juneday/systemetappbasic/PartyTheme.java
@@ -0,0 +1,13 @@
+package se.juneday.systemetappbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartyTheme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party_theme);
+    }
+}
