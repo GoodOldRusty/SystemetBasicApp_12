6f92f17bf189a6e77da4348969f8aa1265e5aaeb
Evelina Bragsjö <gusbragsev@student.gu.se>
2019-05-17 11:16:20 +0200

    trying to commit again with my changes in androidManifest.xml

--- app/src/main/java/se/juneday/systemetappbasic/PartyActivity.java
+++ app/src/main/java/se/juneday/systemetappbasic/PartyActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class PartyTheme extends AppCompatActivity {
+public class PartyActivity extends AppCompatActivity {
 
 
 
@@ -23,28 +23,28 @@ public class PartyTheme extends AppCompatActivity {
         park_party_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent park = new Intent (PartyTheme.this, MainActivity.class);
+                Intent park = new Intent (PartyActivity.this, MainActivity.class);
                 startActivity(park);
             }
         });
         new_year_party_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent new_year = new Intent (PartyTheme.this, MainActivity.class);
+                Intent new_year = new Intent (PartyActivity.this, MainActivity.class);
                 startActivity(new_year);
             }
         });
         exam_party_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent exam = new Intent (PartyTheme.this, MainActivity.class);
+                Intent exam = new Intent (PartyActivity.this, MainActivity.class);
                 startActivity(exam);
             }
         });
         home_party_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent home = new Intent (PartyTheme.this, MainActivity.class);
+                Intent home = new Intent (PartyActivity.this, MainActivity.class);
                 startActivity(home);
             }
         });
